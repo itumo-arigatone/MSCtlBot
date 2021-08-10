a2abe314e70349c70a1bdd08ffcae955143a3ddf
unkomorasi01 <65723443+unkomorasi01@users.noreply.github.com>
2021-08-10 09:54:24 +0900

    Update ec2MinecraftStart.py

--- src/lambda/ec2MinecraftStart.py
+++ src/lambda/ec2MinecraftStart.py
@@ -1,6 +1,6 @@
 import boto3
 region = 'ap-northeast-1'
-instances = ['i-010926c6376db3e72']
+instances = ['xxxxxxxxxx']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
